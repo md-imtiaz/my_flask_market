212a15bd8b3c3febaf6117bbcd930715502565ef
mohammed imtiajul islam <mdimtiazulislam50@gmail.com>
2021-12-28 20:20:46 +0600

    modifi pag.py

--- pag.py
+++ pag.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
